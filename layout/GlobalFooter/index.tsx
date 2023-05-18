f44a4b211978ce46682335d15c2aa951c12f4abb
8ark <sorlti6952@gmail.com>
2023-05-18 14:24:08 +0900

    FECH design lv pages

--- layout/GlobalFooter/index.tsx
+++ layout/GlobalFooter/index.tsx
@@ -21,7 +21,7 @@ export const GlobalFooter = () => {
                 'text-gray-400'
             )}>
                 <a href="#" className='font-extrabold'>근붕트.com</a>은 리부터들을 위한 BynnArk의 모작이며, 작성되어 있는 정보는 실제 정보와 다를 수 있습니다.<br/>
-                오류 제보 또는 문의/피드백은 <b>쿨핀</b>에게 알려주세요.<br/>
+                오류 제보 또는 문의/피드백은 <b>쿨핀</b>에게 알려주세요. thanks to <b className='text-cyan-300'>셜광</b><br/> 
                 copyright 2023. 근붕트.com all rights reserved.<br/>
             </div>
 
