d7d03e2f302b526ab6817d41f529d1431caddebf
8ark <sorlti6952@gmail.com>
2023-05-18 15:28:17 +0900

    FECH main

--- modules/union/Page.tsx
+++ modules/union/Page.tsx
@@ -1,8 +1,40 @@
+import { Container } from '@/components/Container'
+import { Card } from '@geist-ui/core'
+import classNames from 'classnames'
 import React from 'react'
 
 function Page() {
   return (
-    <div>Union Page</div>
+    <Container>
+      <div className={classNames(
+        'w-full',
+
+
+        'flex',
+        'flex-col',
+
+        'gap-y-4',
+
+        'mt-4',
+        'mb-12',
+      )}>
+        <Card>
+          <p className={classNames(
+            'text-sm',
+          )}>💡 입력되는 정보는 <i className={classNames(
+            'font-bold',
+            
+            'decoration-pink-500',
+            'underline',
+          )}>인터넷 기록 삭제 전까지</i> 유효합니다람쥐🐿</p>
+        </Card>
+        <Card>
+          {/* 티어 및 유니온 숫자 */}
+          <div>ss</div>
+          <div>dd</div>
+        </Card>
+      </div>
+    </Container>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,41 @@ import { Container } from "@/components/Container";
 import styled from "@emotion/styled"
 import { Tabs } from "@geist-ui/core";
 import classNames from "classnames";
+import { useEffect, useState } from "react";
 
 
 
 export default function Home() {
+  const [name, setName] = useState('')
+
+  useEffect( () => {
+    //첫 페이지 방문 시 이름 입력
+    if(!localStorage.getItem('name') || localStorage.getItem('name')?.trim() === ''){
+      const nick= prompt('안녕하세요🙌 이름을 알려주실래요? \n한 번 입력하면 못 바꾸니 신중하게 입력해 주세요!')
+      const res:string = nick?nick:''
+      localStorage.setItem('name',res)
+      setName(res)
+
+    }else{
+      setName(localStorage.getItem('name'))
+    }
+  },[])
   return (
     <Container className={classNames(
       'w-full',
-      'h-screen',
-    )}>s
+      'h-96',
+      'my-28',
+
+
+      'flex',
+      'flex-col',
+      'justify-center',
+      'items-center',
+
+      'text-3xl',
+      'text-white',
+      'font-extrabold',
+    )}>{name}님, 안녕하세요 🙌
     </Container>
   )
 }
