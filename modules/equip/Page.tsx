ca0bba3d08ae9a8a5e699e3de966c192de0cb64e
8ark <sorlti6952@gmail.com>
2023-05-22 17:55:59 +0900

    FECH img
    hv to fix img text
    hv to add intial tabs in attack

--- modules/equip/Page.tsx
+++ modules/equip/Page.tsx
@@ -62,7 +62,7 @@ export const Page = () => {
               subtitle="해당 직업 : 제로 은월 키네시스 블래스터 카데나 일리움 아크 패스파인더 호영 아델 카인 라라 칼리 제외한 모든 직업"
             >
               <div className="flex py-10 items-center">
-                <img src="/eq_1.PNG" className={classNames('w-40')} />
+                <img src="/case1.PNG" className={classNames('w-40')} />
                 <span className={classNames('text-xs', 'ml-12', 'font-bold')}>
                   <i className="text-red-200">블랙</i>(반지, 펜던트, 얼굴장식,
                   벨트, 보조무기, 망토)
@@ -75,7 +75,7 @@ export const Page = () => {
             </Collapse>
             <Collapse title="CASE 2 [6블 7트]" subtitle="해당 직업 : 은월">
               <div className="flex py-10 items-center">
-                <img src="/eq_2.PNG" className={classNames('w-40')} />
+                <img src="/case2.PNG" className={classNames('w-40')} />
                 <span className={classNames('text-xs', 'ml-12', 'font-bold')}>
                   <i className="text-red-200">블랙</i>(반지, 펜던트, 얼굴장식,
                   벨트, 모자, 망토)
@@ -88,7 +88,7 @@ export const Page = () => {
             </Collapse>
             <Collapse title="CASE 3 [6블 6트]" subtitle="해당 직업 : 아크 라라">
               <div className="flex py-10 items-center">
-                <img src="/eq_3.PNG" className={classNames('w-40')} />
+                <img src="/case3.PNG" className={classNames('w-40')} />
                 <span className={classNames('text-xs', 'ml-12', 'font-bold')}>
                   <i className="text-red-200">블랙</i>(반지, 펜던트, 얼굴장식,
                   벨트, 모자, 망토)
@@ -104,7 +104,7 @@ export const Page = () => {
               subtitle="해당 직업 : 제로 키네시스 블래스터 카데나 일리움 패스파인더 호영 아델 카인 칼리"
             >
               <div className="flex py-10 items-center">
-                <img src="/eq_4.PNG" className={classNames('w-40')} />
+                <img src="/case4.PNG" className={classNames('w-40')} />
                 <span className={classNames('text-xs', 'ml-12', 'font-bold')}>
                   <i className="text-red-200">블랙</i>(반지, 펜던트, 얼굴장식,
                   벨트, 망토)
